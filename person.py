c9b300264db2766800dd21cbb859c8761ebc48c3
Momodou Keita <Momodou.Keita@tpsgc-pwgsc.gc.ca>
2022-07-04 10:13:31 -0300

    2022-07-04 [Work]

--- person.py
+++
@@ -1,6 +0,0 @@
-class Base:
-      def __init__(self, name, year_born,year_died):
-        self.name = name
-        self.year_born = year_born
-        self.year_died = year_died
-        
--- vessel.py
+++
@@ -1,9 +0,0 @@
-from lib2to3.pytree import Character
-
-class Vessel(Character):
-      def __init__(self, name,year_born, year_died, bonded_with, birthplace,abilities,species,titles
-                   ,profession,groups,nationality,planet,universe,books,ethnicity,spouse,relatives,
-                   acestors,alias,residence,children):
-            super().__init__(name,year_born,year_died,bonded_with, birthplace,abilities,species,titles
-                   ,profession,groups,nationality,planet,universe,books,ethnicity,spouse,relatives,
-                   acestors,alias,residence,children)
